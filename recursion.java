3f5670a877df1c3d591cde1019d1ce04cefd201d
Jackie Lin <jlin00@stuy.edu>
2019-01-31 19:41:49 -0500

    added empty methods and dummy values

---
+++ recursion.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class recursion{
+    /*You may write additional private methods */
+
+    /*Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
+
+    */
+    public static double sqrt(double n, double tolerance){
+      return 0;
+    }
+
+    /*Recursively find the n'th fibbonaci number in linear time
+     *fib(0) = 1; fib(1) = 1; fib(5) = 5
+     *precondition: n is non-negative
+     */
+    public static int fib(int n){
+      return 0;
+    }
+
+    /*As Per classwork*/
+    public static ArrayList<Integer> makeAllSums(){
+      ArrayList<Integer> sum = new ArrayList<Integer>();
+      return sum;
+    }
+
+}
