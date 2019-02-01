71ee49c7f122b73934ab1ff29b5486d8278d747b
Jackie Lin <jlin00@stuy.edu>
2019-01-31 20:54:39 -0500

    added test cases for makeAllSums function

--- recursion.java
+++ recursion.java
@@ -76,7 +76,12 @@ public class recursion{
 
       System.out.println("--------------------------------");
 
-
+      System.out.println("Testing makeAllSums function\n");
+      System.out.println(makeAllSums(1)); //return 0,1
+      System.out.println(makeAllSums(2)); //return 0,1,2,3
+      System.out.println(makeAllSums(3)); //return 0,1,2,3,3,4,5,6
+      System.out.println(makeAllSums(4)); //return 0,1,2,3,3,4,5,6,4,5,6,7,7,8,9,10
+      System.out.println(makeAllSums(0)); //return 0
 
     }
 
